41997cbe96aae8f95360ecc0c8ba62c0268ba632
PRIYAMANSHU KUMAR <123830581+MrPkMehta@users.noreply.github.com>
2024-02-24 09:14:04 +0000

    while and doWhile

---
+++ 05_Iteration/02_whileLoop.js
@@ -0,0 +1,27 @@
+// while loop 
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+//fetching array through while loop
+let myArr = ['pk', 'ak', 'tm', 'hm']
+
+let arr = 0
+while (arr < myArr.length) {
+    console.log(myArr[arr]);
+    arr++
+}
+
+
+
+//do while loop
+// it will execute once either the condition is met or not
+score =1
+do{
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10)
