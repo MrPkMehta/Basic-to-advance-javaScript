76c28f557ddebddfed32425e797413ec405a80f1
PRIYAMANSHU KUMAR <123830581+MrPkMehta@users.noreply.github.com>
2024-02-24 08:00:24 +0000

    truthy and falsy value

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,48 @@
+//basic syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+//for duplicate shift + alt + downArrow
+
+/** key is that value which we have to check everytime hear it is month */
+
+//break is used to stop the control flow of the statement 
+
+
+//if string then we write string in place of integer
+const month = 3 
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;  
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("aprail");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+     case 7:
+        console.log("july");
+        break;
+    default:
+        console.log("enter the valid month");
+        break;
+}
